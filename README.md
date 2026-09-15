6cd867d48439e397be60161cc025e5c4e7f686f8
Artur Fraga Silva <artur.fragas@gmail.com>
2026-09-15 20:56:06 -0300

    Adiciona versão inicial do sistema de estoque

---
+++ README.md
@@ -0,0 +1,9 @@
+Sistema de Estoque.
+
+Sistema de cadastro de produtos, preços e quantidades.
+Construído em Java para práticas de lógica, estruturas de repetição, etc.
+
+Melhorias a serem implementadas:
+- Criação de funções;
+- Conexão com Banco de Dados;
+- Autenticação de Usuário;
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("==================");
+        System.out.println("SISTEMA DE ESTOQUE");
+        System.out.println("==================");
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Quantos produtos deseja cadastrar?");
+        int qtdProdutos = entrada.nextInt();
+
+        double somaGeral = 0;
+
+        for (int i = 1; i < qtdProdutos + 1; i++) {
+            System.out.print("Nome do " + i + "º produto:");
+            String nomeProduto = entrada.next();
+            System.out.print("Preço do produto: R$");
+            double precoProduto = entrada.nextDouble();
+            System.out.print("Quantidade em estoque:");
+            int qtdEstoque = entrada.nextInt();
+
+            double somaProduto = precoProduto * qtdEstoque;
+
+            somaGeral += somaProduto;
+
+        }
+
+        System.out.println("O valor total do estoque é: " + somaGeral);
+        
+
+        entrada.close();
+        
+    }
+}
